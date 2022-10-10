57598e04328653243d4cfb19ae99e50d5f8104bd
zivile <zolynaitezivile@GMAIL.COM>
2022-10-10 13:55:14 +0300

    adding task 2

---
+++ Task 2/script.js
@@ -0,0 +1,12 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+let counter = 0;
+
+document.getElementById('btn__element').addEventListener('click', () => {
+    counter++;
+    document.getElementById('btn__state').innerText = counter;
+});
