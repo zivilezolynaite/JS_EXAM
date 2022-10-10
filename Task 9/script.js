a550e874742974e5c8ff4e85d5308f88637fc12c
zivile <zolynaitezivile@GMAIL.COM>
2022-10-10 14:00:48 +0300

    adding task 9

---
+++ Task 9/script.js
@@ -0,0 +1,25 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kitu atveju false 
+------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget > 100000000) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const answer = new Movie("movieTitle", "movieDirector", 99999);
+console.log(answer.wasExpensive());
